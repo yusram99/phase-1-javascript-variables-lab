b4c470bb198f63c963699114a416a73b9a95d76a
Your Name <youremail@example.com>
2023-06-13 12:14:07 +0300

    commit message

---
+++ Grade.js
@@ -0,0 +1,19 @@
+A = 75 - 100
+B = 60 - 74
+C = 50 - 59
+d = "Try again next time"
+
+let marks =78;
+if (marks>= 75){
+    console. log("A");
+}
+else if(marks >= 60 && marks <= 74 ){
+    console. log("B");
+
+}
+else if(marks >= 50 && marks <= 59){
+    console. log("C");
+}
+else(console. log("Try again next time")
+
+)
---
+++ vote.js
@@ -0,0 +1,7 @@
+//let age=18;
+
+//if (age>=18){
+    console. log("You are allowed to vote!");
+//}else{
+    console.log("You need to be above 18 to vote")
+//}
